b50fcfda85133bb9ea609c7ba4c64f5f2fb1a46e
Eken Özlü <ekenozlu8@gmail.com>
2023-10-15 11:35:02 +0300

    Day2 Commit

---
+++ Day2/LayoutProjesi/LayoutProjesi/DetailsVC.swift
@@ -0,0 +1,22 @@
+//
+//  DetailsVC.swift
+//  LayoutProjesi
+//
+//  Created by Eken Özlü on 15.10.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
---
+++ Day2/LayoutProjesi/LayoutProjesi/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  LayoutProjesi
+//
+//  Created by Eken Özlü on 15.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad Çağırıldı")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear Çağırıldı")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear Çağırıldı")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear Çağırıldı")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear Çağırıldı")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return simpsonArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let simpson = simpsonArray[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = simpson.name
+        content.secondaryText = "Click to see details"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+}
+
